cf145541de2f4f2761e9cf3d9f24e0cec634ac9c
ashfaq1701 <ashfaq.aws@gmail.com>
2021-12-30 12:10:30 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/sortKSortedArray/solution1/sortKSortedArray.kt
+++ src/main/kotlin/ae/hard/sortKSortedArray/solution1/sortKSortedArray.kt
@@ -1,2 +1,39 @@
 package ae.hard.sortKSortedArray.solution1
 
+import java.util.PriorityQueue
+import kotlin.math.min
+
+fun sortKSortedArray(array: MutableList<Int>, k: Int): MutableList<Int> {
+    val minHeapWithKElements = PriorityQueue<Int>()
+
+    // At each index we need to check k elements on it's right
+    // Including itself, k + 1 elements as potential candidate
+    // to be placed in this index.
+    for (i in 0 until min(k + 1, array.size)) {
+        minHeapWithKElements.add(array[i])
+    }
+
+    var nextIndexToInsertElement = 0
+
+    for (i in k + 1 .. array.lastIndex) {
+        // We get the min element fron the heap of k + 1 size
+        // And insert into the result array
+        val minElement = minHeapWithKElements.poll()
+        array[nextIndexToInsertElement] = minElement
+        nextIndexToInsertElement += 1
+
+        // And after that we push the current element
+        // from the array into the heap to keep the heap
+        // k + 1 elements long
+        val currentElement = array[i]
+        minHeapWithKElements.add(currentElement)
+    }
+
+    while (minHeapWithKElements.isNotEmpty()) {
+        val minElement = minHeapWithKElements.poll()
+        array[nextIndexToInsertElement] = minElement
+        nextIndexToInsertElement += 1
+    }
+
+    return array
+}
--- src/main/kotlin/ae/hard/zigzagTraverse/solution1/zigzagTraverse.kt
+++ src/main/kotlin/ae/hard/zigzagTraverse/solution1/zigzagTraverse.kt
@@ -1,2 +1,52 @@
 package ae.hard.zigzagTraverse.solution1
 
+fun zigzagTraverse(array: List<List<Int>>): List<Int> {
+    val lastRow = array.lastIndex
+    val lastCol = array[0].lastIndex
+
+    val result = mutableListOf<Int>()
+    var goingDown = true
+
+    var row = 0
+    var col = 0
+
+    while (!isOutOfBounds(row, col, lastRow, lastCol)) {
+        result.add(array[row][col])
+
+        if (goingDown) {
+
+            if (col == 0 || row == lastRow) {
+                goingDown = false
+
+                if (row == lastRow) {
+                    col += 1
+                } else {
+                    row += 1
+                }
+            } else {
+                row += 1
+                col -= 1
+            }
+        } else {
+
+            if (row == 0 || col == lastCol) {
+                goingDown = true
+
+                if (col == lastCol) {
+                    row += 1
+                } else {
+                    col += 1
+                }
+            } else {
+                row -= 1
+                col += 1
+            }
+        }
+    }
+
+    return result
+}
+
+fun isOutOfBounds(row: Int, col: Int, lastRow: Int, lastCol: Int): Boolean {
+    return row < 0 || row > lastRow || col < 0 || col > lastCol
+}
