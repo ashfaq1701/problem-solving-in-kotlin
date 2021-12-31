1be8c1012d95b9e3190b12bf3f24ef902b43a722
ashfaq1701 <ashfaq.aws@gmail.com>
2021-12-31 22:53:08 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/boggleBoard/solution1/boggleBoard.kt
+++ src/main/kotlin/ae/hard/boggleBoard/solution1/boggleBoard.kt
@@ -1,2 +1,88 @@
 package ae.hard.boggleBoard.solution1
 
+const val DELIMITER = '*'
+
+fun boggleBoard(board: List<List<Char>>, words: List<String>): List<String> {
+    val root = populateSuffixTrie(words)
+
+    val result = mutableSetOf<String>()
+    val visited = MutableList(board.size) { MutableList(board[0].size) { false } }
+
+    for (r in board.indices) {
+        for (c in board[0].indices) {
+            dfsVisitNode(r, c, root, board, visited, result)
+        }
+    }
+
+    return result.toList()
+}
+
+class TrieNode {
+    val children: MutableMap<Char, TrieNode> = mutableMapOf()
+    var word: String = ""
+}
+
+fun populateSuffixTrie(words: List<String>): TrieNode {
+    val root = TrieNode()
+
+    for (word in words) {
+        var currentNode = root
+
+        for (c in word) {
+            if (!currentNode.children.containsKey(c)) {
+                currentNode.children[c] = TrieNode()
+            }
+
+            currentNode = currentNode.children[c]!!
+        }
+
+        val delimiterNode = TrieNode().apply {
+            this.word = word
+        }
+        currentNode.children[DELIMITER] = delimiterNode
+    }
+
+    return root
+}
+
+fun dfsVisitNode(r: Int, c: Int, currentNode: TrieNode, board: List<List<Char>>, visited: MutableList<MutableList<Boolean>>, result: MutableSet<String>) {
+    if (visited[r][c]) return
+
+    val letter = board[r][c]
+
+    if (!currentNode.children.containsKey(letter)) return
+
+    visited[r][c] = true
+
+    val nextNode = currentNode.children[letter]!!
+
+    if (nextNode.children.containsKey(DELIMITER)) {
+        result.add(nextNode.children[DELIMITER]!!.word)
+    }
+
+    val neighbors = getNeighbors(r, c, board)
+
+    for ((neighborRow, neighborCol) in neighbors) {
+        dfsVisitNode(neighborRow, neighborCol, nextNode, board, visited, result)
+    }
+
+    visited[r][c] = false
+}
+
+fun getNeighbors(r: Int, c: Int, board: List<List<Char>>): List<Pair<Int, Int>> {
+    val rows = board.size
+    val cols = board[0].size
+
+    return listOf(
+        r - 1 to c + 1 ,
+        r to c + 1,
+        r + 1 to c + 1,
+        r + 1 to c,
+        r + 1 to c - 1,
+        r to c - 1,
+        r - 1 to c - 1,
+        r - 1 to c
+    ).filter { (row, col) ->
+        row in 0 until rows && col in 0 until cols
+    }
+}
--- src/main/kotlin/ae/hard/maximumSumSubmatrix/solution1/maximumSumSubmatrix.kt
+++ src/main/kotlin/ae/hard/maximumSumSubmatrix/solution1/maximumSumSubmatrix.kt
@@ -1,2 +1,64 @@
 package ae.hard.maximumSumSubmatrix.solution1
 
+import kotlin.math.max
+
+fun maximumSumSubmatrix(matrix: List<List<Int>>, size: Int): Int {
+    // Each element considered to be the bottom right corner of the rectangle
+    // Formula sum = sums[r][c] - sums[r - size][c] - sums[r][c - size] + sums[r - size][c - size]
+    val sumMatrix = createSumMatrix(matrix)
+    var maxSum = Integer.MIN_VALUE
+
+    // i = (size - 1) and j = (size - 1) constitutes the first
+    // size * size rectangle whose bottom right corner is at (i, j)
+    for (r in size - 1 until matrix.size) {
+        for (c in size - 1 until matrix[0].size) {
+            var total = sumMatrix[r][c]
+
+            val touchesTopBorder = r - size < 0
+            if (!touchesTopBorder) {
+                total -= sumMatrix[r - size][c]
+            }
+
+            val touchesLeftBorder = c - size < 0
+            if (!touchesLeftBorder) {
+                total -= sumMatrix[r][c - size]
+            }
+
+            val touchesAnyBorder = touchesTopBorder || touchesLeftBorder
+            if (!touchesAnyBorder) {
+                total += sumMatrix[r - size][c - size]
+            }
+
+            maxSum = max(maxSum, total)
+        }
+    }
+
+    return maxSum
+}
+
+fun createSumMatrix(matrix: List<List<Int>>): List<List<Int>> {
+    val sumMatrix = MutableList(matrix.size) {
+        MutableList(matrix[0].size) { 0 }
+    }
+
+    sumMatrix[0][0] = matrix[0][0]
+
+    // Populate first row
+    for (c in 1 until matrix[0].size) {
+        sumMatrix[0][c] = matrix[0][c] + sumMatrix[0][c - 1]
+    }
+
+    // Populate first column
+    for (r in 1 until matrix.size) {
+        sumMatrix[r][0] = matrix[r][0] + sumMatrix[r - 1][0]
+    }
+
+    // sums[r][c] = matrix[r][c] + sumMatrix[r - 1][c] + sumMatrix[r][c - 1] - sumMatrix[r - 1][c - 1]
+    for (r in 1 until matrix.size) {
+        for (c in 1 until matrix[0].size) {
+            sumMatrix[r][c] = matrix[r][c] + sumMatrix[r - 1][c] + sumMatrix[r][c - 1] - sumMatrix[r - 1][c - 1]
+        }
+    }
+
+    return sumMatrix
+}
