c0f2591378ed9da5c60be0306f7b5c608dd5fc0a
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-04 04:25:49 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/validateThreeNodes/solution1/validateThreeNodes.kt
+++ src/main/kotlin/ae/hard/validateThreeNodes/solution1/validateThreeNodes.kt
@@ -1,2 +1,31 @@
 package ae.hard.validateThreeNodes.solution1
 
+open class BST(value: Int) {
+    var value = value
+    var left: BST? = null
+    var right: BST? = null
+}
+
+fun validateThreeNodes(nodeOne: BST, nodeTwo: BST, nodeThree: BST): Boolean {
+    if (isDescendent(nodeOne, nodeTwo)) {
+        return isDescendent(nodeTwo, nodeThree)
+    }
+
+    if (isDescendent(nodeThree, nodeTwo)) {
+        return isDescendent(nodeTwo, nodeOne)
+    }
+
+    return false
+}
+
+fun isDescendent(node: BST?, target: BST): Boolean {
+    if (node == null) return false
+
+    if (node.value == target.value) return true
+
+    return if (target.value < node.value) {
+        isDescendent(node.left, target)
+    } else {
+        isDescendent(node.right, target)
+    }
+}
--- src/main/kotlin/ae/hard/validateThreeNodes/solution2/validateThreeNodes.kt
+++ src/main/kotlin/ae/hard/validateThreeNodes/solution2/validateThreeNodes.kt
@@ -1,2 +1,33 @@
 package ae.hard.validateThreeNodes.solution2
 
+open class BST(value: Int) {
+    var value = value
+    var left: BST? = null
+    var right: BST? = null
+}
+
+fun validateThreeNodes(nodeOne: BST, nodeTwo: BST, nodeThree: BST): Boolean {
+    if (isDescendent(nodeOne, nodeTwo)) {
+        return isDescendent(nodeTwo, nodeThree)
+    }
+
+    if (isDescendent(nodeThree, nodeTwo)) {
+        return isDescendent(nodeTwo, nodeOne)
+    }
+
+    return false
+}
+
+fun isDescendent(node: BST?, target: BST): Boolean {
+    var currentNode = node
+
+    while (currentNode != null && currentNode != target) {
+        currentNode = if (target.value < currentNode.value) {
+            currentNode.left
+        } else {
+            currentNode.right
+        }
+    }
+
+    return currentNode == target
+}
