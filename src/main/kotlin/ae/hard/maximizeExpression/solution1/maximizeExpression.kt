f374d6576e62a834ed1f67bfadd5e69922c2c25c
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-03 23:16:05 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/maximizeExpression/solution1/maximizeExpression.kt
+++ src/main/kotlin/ae/hard/maximizeExpression/solution1/maximizeExpression.kt
@@ -1,2 +1,33 @@
 package ae.hard.maximizeExpression.solution1
 
+import kotlin.math.max
+
+fun maximizeExpression(array: List<Int>): Int {
+    if (array.size < 4) return 0
+
+    var maxValue = Int.MIN_VALUE
+
+    for (a in 0 .. array.lastIndex) {
+        val aValue = array[a]
+
+        for (b in a + 1 .. array.lastIndex) {
+            val bValue = array[b]
+
+            for (c in b + 1 .. array.lastIndex) {
+                val cValue = array[c]
+
+                for (d in c + 1 .. array.lastIndex) {
+                    val dValue = array[d]
+
+                    maxValue = max(maxValue, evaluateExpress(aValue, bValue, cValue, dValue))
+                }
+            }
+        }
+    }
+
+    return maxValue
+}
+
+fun evaluateExpress(a: Int, b: Int, c: Int, d: Int): Int {
+    return a - b + c - d
+}
--- src/main/kotlin/ae/hard/maximizeExpression/solution2/maximizeExpression.kt
+++ src/main/kotlin/ae/hard/maximizeExpression/solution2/maximizeExpression.kt
@@ -1,2 +1,44 @@
 package ae.hard.maximizeExpression.solution2
 
+import kotlin.math.max
+
+fun maximizeExpression(array: List<Int>): Int {
+    if (array.size < 4) return 0
+
+    val maxOfA = MutableList(array.size) { 0 }
+    maxOfA[0] = array[0]
+
+    val maxOfAMinusB = MutableList(array.size) { 0 }
+    maxOfAMinusB[0] = Int.MIN_VALUE
+
+    val maxOfAMinusBPlusC = MutableList(array.size) { 0 }
+    maxOfAMinusBPlusC[0] = Int.MIN_VALUE
+    maxOfAMinusBPlusC[1] = Int.MIN_VALUE
+
+    val maxOfAMinusBPlusCMinusD = MutableList(array.size) { 0 }
+    maxOfAMinusBPlusCMinusD[0] = Int.MIN_VALUE
+    maxOfAMinusBPlusCMinusD[1] = Int.MIN_VALUE
+    maxOfAMinusBPlusCMinusD[2] = Int.MIN_VALUE
+
+    for (a in 1 .. array.lastIndex) {
+        val currentMax = max(maxOfA[a - 1], array[a])
+        maxOfA[a] = currentMax
+    }
+
+    for (b in 1 .. array.lastIndex) {
+        val currentMax = max(maxOfAMinusB[b - 1], maxOfA[b - 1] - array[b])
+        maxOfAMinusB[b] = currentMax
+    }
+
+    for (c in 2 .. array.lastIndex) {
+        val currentMax = max(maxOfAMinusBPlusC[c - 1], maxOfAMinusB[c - 1] + array[c])
+        maxOfAMinusBPlusC[c] = currentMax
+    }
+
+    for (d in 3 .. array.lastIndex) {
+        val currentMax = max(maxOfAMinusBPlusCMinusD[d - 1], maxOfAMinusBPlusC[d - 1] - array[d])
+        maxOfAMinusBPlusCMinusD[d] = currentMax
+    }
+
+    return maxOfAMinusBPlusCMinusD.last()
+}
