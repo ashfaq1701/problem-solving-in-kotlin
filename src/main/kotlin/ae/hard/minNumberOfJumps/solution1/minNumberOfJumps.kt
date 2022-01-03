08079aae50f6912bfe9400ef543f5950057dedb7
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-03 23:35:19 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/minNumberOfJumps/solution1/minNumberOfJumps.kt
+++ src/main/kotlin/ae/hard/minNumberOfJumps/solution1/minNumberOfJumps.kt
@@ -1,2 +1,18 @@
 package ae.hard.minNumberOfJumps.solution1
 
+import kotlin.math.min
+
+fun minNumberOfJumps(array: List<Int>): Int {
+    val jumps = MutableList(array.size) { Int.MAX_VALUE }
+    jumps[0] = 0
+
+    for (i in 0 until array.size) {
+        for (j in 0 until i) {
+            if (j + array[j] >= i) {
+                jumps[i] = min(jumps[i], jumps[j] + 1)
+            }
+        }
+    }
+
+    return jumps.last()
+}
--- src/main/kotlin/ae/hard/minNumberOfJumps/solution2/minNumberOfJumps.kt
+++ src/main/kotlin/ae/hard/minNumberOfJumps/solution2/minNumberOfJumps.kt
@@ -1,2 +1,24 @@
 package ae.hard.minNumberOfJumps.solution2
 
+import kotlin.math.max
+
+fun minNumberOfJumps(array: List<Int>): Int {
+
+    if (array.size == 1) return 0
+
+    var jumps = 0
+    var maxReach = array[0]
+    var steps = array[0]
+
+    for (i in 1 until array.lastIndex) {
+        maxReach = max(maxReach, i + array[i])
+        steps -= 1
+
+        if (steps == 0) {
+            jumps += 1
+            steps = maxReach - i
+        }
+    }
+
+    return jumps + 1
+}
