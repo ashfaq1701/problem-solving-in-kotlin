d1770ca30697a14a752a8fcf026b9924a7bade85
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-04 03:02:44 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/sameBSTs/solution1/sameBSTs.kt
+++ src/main/kotlin/ae/hard/sameBSTs/solution1/sameBSTs.kt
@@ -1,2 +1,28 @@
 package ae.hard.sameBSTs.solution1
 
+fun sameBsts(arrayOne: List<Int>, arrayTwo: List<Int>): Boolean {
+    if (arrayOne.size != arrayTwo.size) return false
+
+    if (arrayOne.isEmpty() && arrayTwo.isEmpty()) return true
+
+    if (arrayOne[0] != arrayTwo[0]) return false
+
+    val leftOne = getSmaller(arrayOne)
+    val rightOne = getGreaterThanOrEqualTo(arrayOne)
+
+    val leftTwo = getSmaller(arrayTwo)
+    val rightTwo = getGreaterThanOrEqualTo(arrayTwo)
+
+    return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo)
+}
+
+fun getSmaller(array: List<Int>): List<Int> {
+    val rootValue = array[0]
+    return array.subList(1, array.size).filter { it < rootValue }
+}
+
+fun getGreaterThanOrEqualTo(array: List<Int>): List<Int> {
+    val rootValue = array[0]
+    return array.subList(1, array.size).filter { it >= rootValue }
+}
+
--- src/main/kotlin/ae/hard/sameBSTs/solution2/sameBSTs.kt
+++ src/main/kotlin/ae/hard/sameBSTs/solution2/sameBSTs.kt
@@ -1,2 +1,47 @@
 package ae.hard.sameBSTs.solution2
 
+fun sameBsts(arrayOne: List<Int>, arrayTwo: List<Int>): Boolean {
+    return areSameBsts(arrayOne, arrayTwo, 0, 0, Int.MIN_VALUE, Int.MAX_VALUE)
+}
+
+fun areSameBsts(arrayOne: List<Int>, arrayTwo: List<Int>, rootIdxOne: Int, rootIdxTwo: Int, minVal: Int, maxVal: Int): Boolean {
+    if (rootIdxOne == -1 || rootIdxTwo == -1) {
+        return rootIdxOne == rootIdxTwo
+    }
+
+    if (arrayOne[rootIdxOne] != arrayTwo[rootIdxTwo]) {
+        return false
+    }
+
+    val leftIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal)
+    val leftIdxTwo = getIdxOfFirstSmaller(arrayTwo, rootIdxTwo, minVal)
+
+    val rightIdxOne = getIdxOfFirstGreaterTnanOrEqual(arrayOne, rootIdxOne, maxVal)
+    val rightIdxTwo = getIdxOfFirstGreaterTnanOrEqual(arrayTwo, rootIdxTwo, maxVal)
+
+    val currentVal = arrayOne[rootIdxOne]
+    val areLeftSame = areSameBsts(arrayOne, arrayTwo, leftIdxOne, leftIdxTwo, minVal, currentVal)
+    val areRightSame = areSameBsts(arrayOne, arrayTwo, rightIdxOne, rightIdxTwo, currentVal, maxVal)
+
+    return areLeftSame && areRightSame
+}
+
+fun getIdxOfFirstSmaller(array: List<Int>, rootIdx: Int, minVal: Int): Int {
+    for (i in rootIdx + 1 .. array.lastIndex) {
+        if (array[i] < array[rootIdx] && array[i] >= minVal) {
+            return i
+        }
+    }
+
+    return -1
+}
+
+fun getIdxOfFirstGreaterTnanOrEqual(array: List<Int>, rootIdx: Int, maxVal: Int): Int {
+    for (i in rootIdx + 1 .. array.lastIndex) {
+        if (array[i] >= array[rootIdx] && array[i] < maxVal) {
+            return i
+        }
+    }
+
+    return -1
+}
