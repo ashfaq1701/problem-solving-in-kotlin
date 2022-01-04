4d6afeb8af9b168a58b9eccdf65d20af0dc58017
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-04 15:16:59 +0700

    Problem solution added

--- src/main/kotlin/ae/hard/multiStringSearch/solution1/multiStringSearch.kt
+++ src/main/kotlin/ae/hard/multiStringSearch/solution1/multiStringSearch.kt
@@ -1,2 +1,43 @@
 package ae.hard.multiStringSearch.solution1
 
+// O(bns) Time | O(n) Space
+fun multiStringSearch(bigString: String, smallStrings: List<String>): List<Boolean> {
+    return smallStrings.map { smallString ->
+        isInBigString(bigString, smallString)
+    }
+}
+
+fun isInBigString(bigString: String, smallString: String): Boolean {
+    for (i in bigString.indices) {
+        if (i + smallString.length > bigString.length) {
+            break
+        }
+
+        if (isInBigStringHelper(bigString, smallString, i)) {
+            return true
+        }
+    }
+
+    return false
+}
+
+fun isInBigStringHelper(bigString: String, smallString: String, startIdx: Int): Boolean {
+    var bigLeftIdx = startIdx
+    var bigRightIdx = startIdx + smallString.length - 1
+
+    var smallLeftIdx = 0
+    var smallRightIdx = smallString.lastIndex
+
+    while (bigLeftIdx < bigRightIdx) {
+        if (bigString[bigLeftIdx] != smallString[smallLeftIdx] || bigString[bigRightIdx] != smallString[smallRightIdx]) {
+            return false
+        }
+
+        bigLeftIdx += 1
+        smallLeftIdx += 1
+        bigRightIdx -= 1
+        smallRightIdx -= 1
+    }
+
+    return true
+}
--- src/main/kotlin/ae/hard/multiStringSearch/solution2/multiStringSearch.kt
+++ src/main/kotlin/ae/hard/multiStringSearch/solution2/multiStringSearch.kt
@@ -1,2 +1,57 @@
 package ae.hard.multiStringSearch.solution2
 
+fun multiStringSearch(bigString: String, smallStrings: List<String>): List<Boolean> {
+    val suffixTrie = ModifiedSuffixTree(bigString)
+
+    return smallStrings.map { smallString ->
+        suffixTrie.contains(smallString)
+    }
+}
+
+class TrieNode {
+    val children = mutableMapOf<Char, TrieNode>()
+}
+
+class ModifiedSuffixTree(val str: String) {
+    val rootNode = TrieNode()
+
+    init {
+        populateSuffixTrie(str)
+    }
+
+    fun populateSuffixTrie(str: String) {
+        for (i in str.indices) {
+            insertSubstringStartingAt(str, i)
+        }
+    }
+
+    fun insertSubstringStartingAt(str: String, idx: Int) {
+        var currentNode = rootNode
+
+        for (j in idx .. str.lastIndex) {
+            val letter = str[j]
+
+            if (!currentNode.children.containsKey(letter)) {
+                currentNode.children[letter] = TrieNode()
+            }
+
+            currentNode = currentNode.children[letter]!!
+        }
+    }
+
+    fun contains(str: String): Boolean {
+        var currentNode = rootNode
+
+        for (i in str.indices) {
+            val letter = str[i]
+
+            if (!currentNode.children.containsKey(letter)) {
+                return false
+            }
+
+            currentNode = currentNode.children[letter]!!
+        }
+
+        return true
+    }
+}
