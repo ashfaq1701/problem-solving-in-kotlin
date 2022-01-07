acf971be00c0779990567900c69d34e700e6c307
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-08 01:52:15 +0700

    Added two problem descriptions

---
+++ src/main/kotlin/mocks/interview/pairsOfPrefixOrSuffix/solution1/pairsOfPrefixOrSuffix.kt
@@ -0,0 +1,5 @@
+package mocks.interview.pairsOfPrefixOrSuffix.solution1
+
+/**
+ * Given a list of words, find all the pair of words for which one of them is a prefix or suffix of the other.
+ */
---
+++ src/main/kotlin/mocks/interview/wordSubstitution/solution1/wordSubstitution.kt
@@ -0,0 +1,6 @@
+package mocks.interview.wordSubstitution.solution1
+
+/**
+ * Given a list of words and a substitution map transform the given list of words with the given substitution map.
+ * The substitution should be idempotent. In other words multiple calls of the function should produce the same output.
+ */
