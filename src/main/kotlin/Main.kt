54bcfb7c63575ea7ac37951e403e394f4aeaa816
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-09 01:38:11 +0700

    Problem solution added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,3 @@
+import mocks.interview.pairsOfPrefixOrSuffix.solution1.findPrefixOrSuffixPairs
+
 fun main(args: Array<String>) {}
--- src/main/kotlin/mocks/interview/pairsOfPrefixOrSuffix/solution1/pairsOfPrefixOrSuffix.kt
+++ src/main/kotlin/mocks/interview/pairsOfPrefixOrSuffix/solution1/pairsOfPrefixOrSuffix.kt
@@ -3,3 +3,72 @@ package mocks.interview.pairsOfPrefixOrSuffix.solution1
 /**
  * Given a list of words, find all the pair of words for which one of them is a prefix or suffix of the other.
  */
+
+// val testCase = listOf("an", "ana", "anar", "anarchy", "robolectic", "robo", "ro", "row", "dim", "dima", "duma", "dumas", "xenial", "rock", "rockey", "chunk", "chadder", "adder", "der", "uma", "im", "o", "ow", "archy", "ar")
+
+fun findPrefixOrSuffixPairs(words: List<String>): List<Pair<String, String>> {
+    val trie = Trie()
+    val reverseTrie = Trie()
+
+    for (word in words) {
+        trie.insert(word, true)
+        reverseTrie.insert(word, false)
+    }
+
+    val wordPairs = mutableListOf<Pair<String, String>>()
+    getNextWords(trie.root, wordPairs)
+    getNextWords(reverseTrie.root, wordPairs)
+
+    return wordPairs
+}
+
+fun getNextWords(node: TrieNode, wordPairs: MutableList<Pair<String, String>>): List<String> {
+    val nextWords = mutableListOf<String>()
+
+    val nextNonDelimiterChildren = node.children.filterKeys { it != DELIMITER }
+
+    for ((_, child) in nextNonDelimiterChildren) {
+        nextWords.addAll(getNextWords(child, wordPairs))
+    }
+
+    if (DELIMITER in node.children) {
+        val wordEndingHere = node.children[DELIMITER]!!.word!!
+        for (nextWord in nextWords) {
+            wordPairs.add(wordEndingHere to nextWord)
+        }
+        nextWords.add(wordEndingHere)
+    }
+
+    return nextWords
+}
+
+const val DELIMITER = '*'
+
+class TrieNode {
+    val children = mutableMapOf<Char, TrieNode>()
+    var word: String? = null
+}
+
+class Trie {
+    val root = TrieNode()
+
+    fun insert(word: String, leftToRight: Boolean) {
+        var currentNode = root
+
+        val range = if (leftToRight) word.indices else word.indices.reversed()
+
+        for (idx in range) {
+            val ch = word[idx]
+
+            if (ch !in currentNode.children) {
+                currentNode.children[ch] = TrieNode()
+            }
+
+            currentNode = currentNode.children[ch]!!
+        }
+
+        currentNode.children[DELIMITER] = TrieNode().also {
+            it.word = word
+        }
+    }
+}
