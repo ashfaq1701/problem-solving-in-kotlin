f059fe2f8aa0ad098c8ef372c7e161a0b0cb8e0f
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-11 21:11:12 +0700

    Added formula and explanations

--- src/main/kotlin/ae/veryHard/maxProfitWithKTransactions/solution1/maxProfitWithKTransactions.kt
+++ src/main/kotlin/ae/veryHard/maxProfitWithKTransactions/solution1/maxProfitWithKTransactions.kt
@@ -2,6 +2,24 @@ package ae.veryHard.maxProfitWithKTransactions.solution1
 
 import kotlin.math.max
 
+/**
+ * prices ->   [ 5, 11,  3, 50, 60, 90 ]
+ *           0   0   0   0   0   0   0
+ * profit -> 1   0   6   6  47  57  87
+ *           2   0   6   6  53  63
+ *
+ * profit[t][d] = max { 1) profit[t][d - 1]
+ *                      2) prices[d] + max(-prices[x] + profit[t - 1][x])
+ *                                     0 <= x < d
+ *
+ *                           Profit of 1 day before with same (t) number of transactions
+ *                       1)                     profit[t][d - 1]
+ *                                      Price of current day (income)                     Price of xth day (bought at this price, expense)   Profit till xth day with one less (t - 1) number of transactions
+ * profit[t][d] = max {  2)                     prices[d]            +             max(                    -prices[x]                       +                    profit[t - 1][x]                              )
+ *                                                         This will be calculated from 0th day till the last day (d - 1)
+ *                                                                                 0 <= x < d
+ */
+
 fun maxProfitWithKTransactions(prices: List<Int>, k: Int): Int {
     if (prices.isEmpty()) return 0
 
