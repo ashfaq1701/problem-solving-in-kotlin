208d1b12a3cca3aba1d5aa64f4c341a50c18f87f
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-20 14:32:15 +0700

    Problem solution added

---
+++ src/main/kotlin/leetcode/longestRepeatingCharacterSequence/solution1/longestRepeatingCharacterSequence.kt
@@ -0,0 +1,30 @@
+package leetcode.longestRepeatingCharacterSequence.solution1
+
+import kotlin.math.max
+
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val charCounts = MutableList(26) { 0 }
+        var left = 0
+        var right = 0
+        var maxCount = 0
+        var maxLength = 0
+
+        while (right < s.length) {
+
+            charCounts[s[right] - 'A'] += 1
+            maxCount = max(maxCount,  charCounts[s[right] - 'A'])
+
+            while (right - left + 1 - maxCount > k) {
+                charCounts[s[left] - 'A'] -= 1
+                maxCount = max(maxCount,  charCounts[s[left] - 'A'])
+                left += 1
+            }
+
+            maxLength = max(maxLength, right - left + 1)
+            right += 1
+        }
+
+        return maxLength
+    }
+}
