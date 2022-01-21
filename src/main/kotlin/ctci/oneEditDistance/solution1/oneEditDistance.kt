1aa494037f35687464d6f4d94817fe69adac7326
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-21 19:26:56 +0700

    Problem solution added

--- src/main/kotlin/ctci/oneEditDistance/solution1/oneEditDistance.kt
+++ src/main/kotlin/ctci/oneEditDistance/solution1/oneEditDistance.kt
@@ -1,2 +1,35 @@
 package ctci.oneEditDistance.solution1
 
+import kotlin.math.min
+
+class Solution {
+    fun isOneEditDistance(s: String, t: String): Boolean {
+        var i = 0
+
+        while (i < min(s.length, t.length) && s[i] == t[i]) {
+            i += 1
+        }
+
+        if (i == s.length && i == t.length) return false
+
+        return areSameStartingAt(s, t, i + 1, i) || areSameStartingAt(s, t, i, i + 1) || areSameStartingAt(s, t, i + 1, i + 1)
+    }
+
+    fun areSameStartingAt(s: String, t: String, sIdx: Int, tIdx: Int): Boolean {
+        if (s.length - sIdx != t.length - tIdx) return false
+
+        // s.length = 7, sIdx = 3
+        // 7 - 3 = 4
+        // 0 until 4
+        // 0 1 2 3
+        // sIdx + i
+        // 3 4 5 6
+        for (i in 0 until s.length - sIdx) {
+            if (s[sIdx + i] != t[tIdx + i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
--- src/main/kotlin/ctci/palindromePermutation/solution1/palindromePermutation.kt
+++ src/main/kotlin/ctci/palindromePermutation/solution1/palindromePermutation.kt
@@ -1,2 +1,15 @@
 package ctci.palindromePermutation.solution1
 
+class Solution {
+    fun canPermutePalindrome(s: String): Boolean {
+        val charCounts = MutableList(26) { 0 }
+
+        for (c in s) {
+            charCounts[c - 'a'] += 1
+        }
+
+        val oddCharCounts = charCounts.count { it % 2 == 1 }
+
+        return oddCharCounts <= 1
+    }
+}
