93d85f4331ae870e52ff971f170b8b9abd733a75
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-25 20:54:44 +0700

    Problem solution added

--- src/main/kotlin/ctci/removeNthNodeFromEndOfList/solution1/removeNthNodeFromEndOfList.kt
+++ src/main/kotlin/ctci/removeNthNodeFromEndOfList/solution1/removeNthNodeFromEndOfList.kt
@@ -1,2 +1,29 @@
 package ctci.removeNthNodeFromEndOfList.solution1
 
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        val preHead = ListNode(-1)
+        preHead.next = head
+
+        var slow: ListNode? = preHead
+        var fast: ListNode? = preHead
+
+        for (currentPosition in 1 .. n) {
+            if (fast == null) break
+            fast = fast.next
+        }
+
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next
+        }
+
+        slow?.next = slow?.next?.next
+
+        return preHead.next
+    }
+}
