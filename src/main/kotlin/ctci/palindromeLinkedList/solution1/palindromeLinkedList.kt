11fc8fdbfe176fdac28a370ea248184d8dd33cb9
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-26 14:55:25 +0700

    Problem solution added

--- src/main/kotlin/ctci/palindromeLinkedList/solution1/palindromeLinkedList.kt
+++ src/main/kotlin/ctci/palindromeLinkedList/solution1/palindromeLinkedList.kt
@@ -1,2 +1,21 @@
 package ctci.palindromeLinkedList.solution1
 
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun isPalindrome(head: ListNode?): Boolean {
+        return isPalindromeRecursive(head, head).first
+    }
+
+    fun isPalindromeRecursive(leftNode: ListNode?, rightNode: ListNode?): Pair<Boolean, ListNode?> {
+        if (rightNode == null) return true to leftNode
+
+        val (areOuterNodesEqual, leftNodeToCompare) = isPalindromeRecursive(leftNode, rightNode.next)
+
+        val areNodesEqual = areOuterNodesEqual && leftNodeToCompare!!.`val` == rightNode.`val`
+
+        return areNodesEqual to leftNodeToCompare?.next
+    }
+}
