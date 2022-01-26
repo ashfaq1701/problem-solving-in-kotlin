0926d0d57a359a38f96eaceef688c87035661daf
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-26 20:35:47 +0700

    Problem solution added

--- src/main/kotlin/ctci/minStack/solution1/minStack.kt
+++ src/main/kotlin/ctci/minStack/solution1/minStack.kt
@@ -1,2 +1,31 @@
 package ctci.minStack.solution1
 
+import kotlin.math.min
+
+class MinStack() {
+
+    private val stk = mutableListOf<Int>()
+    private val minElements = mutableListOf<Int>()
+
+    fun push(`val`: Int) {
+        minElements.add(
+            if (minElements.isEmpty()) `val` else min(`val`, minElements.last())
+        )
+
+        stk.add(`val`)
+    }
+
+    fun pop() {
+        stk.removeAt(stk.lastIndex)
+        minElements.removeAt(minElements.lastIndex)
+    }
+
+    fun top(): Int {
+        return stk.last()
+    }
+
+    fun getMin(): Int {
+        return minElements.last()
+    }
+
+}
