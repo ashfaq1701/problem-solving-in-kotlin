d9d9e756582e1a2775ac2d0dfad10492e1741d9d
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-26 17:37:37 +0700

    Added comments

--- src/main/kotlin/ctci/intersectionOfTwoLinkedLists/solution1/intersectionOfTwoLinkedLists.kt
+++ src/main/kotlin/ctci/intersectionOfTwoLinkedLists/solution1/intersectionOfTwoLinkedLists.kt
@@ -1,2 +1,43 @@
 package ctci.intersectionOfTwoLinkedLists.solution1
 
+import kotlin.math.abs
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        val len1 = getLength(headA)
+        val len2 = getLength(headB)
+
+        val biggerList = if (len1 > len2) headA else headB
+        val smallerList = if (biggerList == headA) headB else headA
+
+        return getIntersectionHelper(biggerList, smallerList, abs(len1 - len2))
+    }
+
+    fun getIntersectionHelper(biggerList: ListNode?, smallerList: ListNode?, diff: Int): ListNode? {
+        return if (diff > 0) {
+            getIntersectionHelper(biggerList?.next, smallerList, diff - 1)
+        } else {
+            if (biggerList == smallerList) {
+                biggerList
+            } else {
+                getIntersectionHelper(biggerList?.next, smallerList?.next, diff)
+            }
+        }
+    }
+
+    fun getLength(head: ListNode?): Int {
+        var current = head
+        var length = 0
+
+        while (current != null) {
+            current = current.next
+            length += 1
+        }
+
+        return length
+    }
+}
--- src/main/kotlin/ctci/linkedListCycleII/solution1/linkedListCycleII.kt
+++ src/main/kotlin/ctci/linkedListCycleII/solution1/linkedListCycleII.kt
@@ -1,2 +1,29 @@
 package ctci.linkedListCycleII.solution1
 
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun detectCycle(head: ListNode?): ListNode? {
+        var slow = head?.next
+        var fast = head?.next?.next
+
+        while (fast != null && slow != fast) {
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+
+        if (fast == null) return null
+
+        var first = head
+        var second = fast
+
+        while (first != second) {
+            first = first?.next
+            second = second?.next
+        }
+
+        return first
+    }
+}
