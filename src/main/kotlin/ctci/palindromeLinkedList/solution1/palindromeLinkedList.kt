3bc7104a0ca520fb69da7a3d7400fdc52efac1f9
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-26 15:52:17 +0700

    Added comments

--- src/main/kotlin/ctci/palindromeLinkedList/solution1/palindromeLinkedList.kt
+++ src/main/kotlin/ctci/palindromeLinkedList/solution1/palindromeLinkedList.kt
@@ -4,6 +4,28 @@ class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
 
+/**
+ * a -> b -> c -> b -> a
+ *
+ * a, a    - a == a, return a.next = (true, null)
+ * a, b    - b == b, return b.next = (true, a)
+ * a, c    - c == c, return c.next = (true, b)
+ * a, b    - b == b, return b.next = (true, c)
+ * a, a    - a == a, return a.next = (true, b)
+ * a, null -         return (true, a)
+ */
+
+/**
+ * a -> b -> c -> c -> b -> a
+ * a, a    - a == a, return a.next = (true, null)
+ * a, b    - b == b, return b.next = (true, a)
+ * a, c    - c == c, return c.next = (true, b)
+ * a, c    - c == c, return c.next = (true, c)
+ * a, b    - b == b, return b.next = (true, c)
+ * a, a    - a == a, return a.next = (true, b)
+ * a, null -         return (true, a)
+ */
+
 class Solution {
     fun isPalindrome(head: ListNode?): Boolean {
         return isPalindromeRecursive(head, head).first
