7211d45f0ec5e45242f5d090890f08e74fd9efee
ashfaq1701 <ashfaq.aws@gmail.com>
2022-01-29 01:29:01 +0700

    Problem solution added

--- src/main/kotlin/ctci/inorderSuccessorInBSTII/solution1/inorderSuccessorInBSTII.kt
+++ src/main/kotlin/ctci/inorderSuccessorInBSTII/solution1/inorderSuccessorInBSTII.kt
@@ -1,2 +1,39 @@
 package ctci.inorderSuccessorInBSTII.solution1
 
+class Node(var `val`: Int) {
+    var left: Node? = null
+    var right: Node? = null
+    var parent: Node? = null
+}
+
+class Solution {
+    fun inorderSuccessor(node: Node?): Node? {
+        if (node == null) return null
+
+        return if (node.right != null) {
+            getLeftMostChild(node.right!!)
+        } else {
+            getRightMostParent(node)
+        }
+    }
+
+    fun getLeftMostChild(node: Node): Node {
+        var current = node
+
+        while (current.left != null) {
+            current = current.left!!
+        }
+
+        return current
+    }
+
+    fun getRightMostParent(node: Node): Node? {
+        var current = node
+
+        while (current.parent != null && current.parent!!.right == current) {
+            current = current.parent!!
+        }
+
+        return current.parent
+    }
+}
