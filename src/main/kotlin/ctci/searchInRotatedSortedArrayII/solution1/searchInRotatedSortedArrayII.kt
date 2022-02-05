d4f684e25c0cc3935e8b36dcb3f1cc262d2c8194
ashfaq1701 <ashfaq.aws@gmail.com>
2022-02-06 02:05:54 +0700

    Problem solution added

--- src/main/kotlin/ctci/searchInRotatedSortedArrayII/solution1/searchInRotatedSortedArrayII.kt
+++ src/main/kotlin/ctci/searchInRotatedSortedArrayII/solution1/searchInRotatedSortedArrayII.kt
@@ -1,2 +1,36 @@
 package ctci.searchInRotatedSortedArrayII.solution1
 
+class Solution {
+    fun search(nums: IntArray, target: Int): Boolean {
+        var start = 0
+        var end = nums.lastIndex
+
+        while (start <= end) {
+            val mid = (start + end) / 2
+
+            if (nums[mid] == target) {
+                return true
+            } else if (nums[mid] == nums[start]) {
+                // If mid element and start elements are equal
+                // Then mid can be both in first or second half
+                // This is a undeterministic condition
+                // Search window will start from `start + 1` now
+                start += 1
+            } else if (nums[mid] > nums[start]) {
+                if (target in nums[start] .. nums[mid]) {
+                    end = mid - 1
+                } else {
+                    start = mid + 1
+                }
+            } else {
+                if (target in nums[mid] .. nums[end]) {
+                    start = mid + 1
+                } else {
+                    end = mid - 1
+                }
+            }
+        }
+
+        return false
+    }
+}
--- src/main/kotlin/ctci/searchInSortedArrayOfUnknownSize/solution1/searchInSortedArrayOfUnknownSize.kt
+++ src/main/kotlin/ctci/searchInSortedArrayOfUnknownSize/solution1/searchInSortedArrayOfUnknownSize.kt
@@ -1,2 +1,32 @@
 package ctci.searchInSortedArrayOfUnknownSize.solution1
 
+class ArrayReader {
+    fun get(index: Int): Int { return 0 }
+}
+
+class Solution {
+    fun search(reader: ArrayReader, target: Int): Int {
+        var left = 0
+        var right = 1
+
+        while (reader.get(right) < target) {
+            left = right
+            // Or right = right * 2
+            right = right shl 2
+        }
+
+        while (left <= right) {
+            val mid = (left + right) / 2
+
+            if (reader.get(mid) == target) {
+                return mid
+            } else if (target < reader.get(mid)) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return -1
+    }
+}
