64b00f26bc4db12119142519ddcc5c4a19693ea7
ashfaq1701 <ashfaq.aws@gmail.com>
2022-02-09 21:33:34 +0700

    Problem solution added

--- src/main/kotlin/ctci/lruCache/solution1/lruCache.kt
+++ src/main/kotlin/ctci/lruCache/solution1/lruCache.kt
@@ -1,2 +1,100 @@
 package ctci.lruCache.solution1
 
+class LRUCache(capacity: Int) {
+
+    private val maxCapacity: Int
+    private val nodeList = DoublyLinkedList()
+    private val cache = mutableMapOf<Int, DoublyLinkedListNode>()
+
+    init {
+        maxCapacity = capacity
+    }
+
+    fun get(key: Int): Int {
+        if (key !in cache) return -1
+        val node = cache[key]!!
+        updateMostRecent(node)
+        return node.value
+    }
+
+    fun put(key: Int, value: Int) {
+        if (key !in cache) {
+            if (cache.size == maxCapacity) {
+                evictLeastRecent()
+            }
+            cache[key] = DoublyLinkedListNode(key, value)
+        } else {
+            cache[key]!!.value = value
+        }
+
+        updateMostRecent(cache[key]!!)
+    }
+
+    fun updateMostRecent(node: DoublyLinkedListNode) {
+        nodeList.setHeadTo(node)
+    }
+
+    fun evictLeastRecent() {
+        val leastRecentKey = nodeList.tail?.key ?: return
+
+        nodeList.removeTail()
+        cache.remove(leastRecentKey)
+    }
+}
+
+class DoublyLinkedList {
+    var head: DoublyLinkedListNode? = null
+    var tail: DoublyLinkedListNode? = null
+
+    fun setHeadTo(node: DoublyLinkedListNode) {
+        if (node == head) return
+
+        if (head == null) {
+            head = node
+            tail = node
+        } else if (head == tail) {
+            node.next = head
+            head!!.prev = node
+            head = node
+        } else {
+            if (node == tail) {
+                removeTail()
+            }
+
+            node.removeBindings()
+            node.next = head
+            head!!.prev = node
+            head = node
+        }
+    }
+
+    fun removeTail() {
+        if (head == tail) {
+            head = null
+            tail = null
+            return
+        }
+
+        val prev = tail!!.prev
+        tail!!.removeBindings()
+        tail = prev
+    }
+}
+
+class DoublyLinkedListNode(val key: Int, var value: Int) {
+    var prev: DoublyLinkedListNode? = null
+    var next: DoublyLinkedListNode? = null
+
+    fun removeBindings() {
+        if (prev != null) {
+            prev!!.next = next
+        }
+
+        if (next != null) {
+            next!!.prev = prev
+        }
+
+        prev = null
+        next = null
+    }
+}
--- src/main/kotlin/ctci/shortestUnsortedContinuousSubarray/solution1/shortestUnsortedContinuousSubarray.kt
+++ src/main/kotlin/ctci/shortestUnsortedContinuousSubarray/solution1/shortestUnsortedContinuousSubarray.kt
@@ -1,2 +1,49 @@
 package ctci.shortestUnsortedContinuousSubarray.solution1
 
+import kotlin.math.min
+import kotlin.math.max
+
+class Solution {
+    fun findUnsortedSubarray(nums: IntArray): Int {
+        val (minUnsorted, maxUnsorted) = getMinMaxUnsortedNumbers(nums)
+
+        if (minUnsorted == Int.MAX_VALUE) return 0
+
+        var sortedPositionOfMinUnsorted = 0
+        while (nums[sortedPositionOfMinUnsorted] <= minUnsorted) {
+            sortedPositionOfMinUnsorted += 1
+        }
+
+        var sortedPositionOfMaxUnsorted = nums.lastIndex
+        while (nums[sortedPositionOfMaxUnsorted] >= maxUnsorted) {
+            sortedPositionOfMaxUnsorted -= 1
+        }
+
+        return sortedPositionOfMaxUnsorted - sortedPositionOfMinUnsorted + 1
+    }
+
+    fun getMinMaxUnsortedNumbers(nums: IntArray): Pair<Int, Int> {
+        var maxUnsorted = Int.MIN_VALUE
+        var minUnsorted = Int.MAX_VALUE
+
+        for (i in nums.indices) {
+            if (isUnsorted(nums, i)) {
+                maxUnsorted = max(maxUnsorted, nums[i])
+                minUnsorted = min(minUnsorted, nums[i])
+            }
+        }
+
+        return minUnsorted to maxUnsorted
+    }
+
+    fun isUnsorted(nums: IntArray, idx: Int): Boolean {
+        return if (idx == 0) {
+            idx + 1 < nums.size && nums[idx] > nums[idx + 1]
+        } else if (idx == nums.lastIndex) {
+            idx - 1 >= 0 && nums[idx] < nums[idx - 1]
+        } else {
+            (idx - 1 >= 0 && nums[idx] < nums[idx - 1]) ||
+                    (idx + 1 < nums.size && nums[idx] > nums[idx + 1])
+        }
+    }
+}
