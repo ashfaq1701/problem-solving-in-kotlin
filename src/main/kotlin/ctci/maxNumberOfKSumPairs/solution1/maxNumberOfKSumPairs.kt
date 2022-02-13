ece61f960b079071bdd55ef27cc78e4c969fdaec
ashfaq1701 <ashfaq.aws@gmail.com>
2022-02-13 18:02:07 +0700

    Problem solution added

--- src/main/kotlin/ctci/maxNumberOfKSumPairs/solution1/maxNumberOfKSumPairs.kt
+++ src/main/kotlin/ctci/maxNumberOfKSumPairs/solution1/maxNumberOfKSumPairs.kt
@@ -1,2 +1,22 @@
 package ctci.maxNumberOfKSumPairs.solution1
 
+class Solution {
+    fun maxOperations(nums: IntArray, k: Int): Int {
+        val seen = mutableMapOf<Int, Int>()
+        var operationCount = 0
+
+        for (num in nums) {
+            if ((k - num) in seen && seen[k - num]!! > 0) {
+                operationCount += 1
+                seen[k - num] = seen[k - num]!! - 1
+            } else {
+                if (num !in seen) {
+                    seen[num] = 0
+                }
+                seen[num] = seen[num]!! + 1
+            }
+        }
+
+        return operationCount
+    }
+}
